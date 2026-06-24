d593824eaefc6aacc34567148e6c4c1eb533446e
wangxinpeng.samael__dcar <wangxinpeng.samael@dcarlife.com>
2026-06-24 21:44:28 +0800

    init repo

---
+++ init.lua
@@ -0,0 +1,155 @@
+vim.cmd.source("~/.vimrc")
+vim.pack.add({
+  { src = "https://github.com/olimorris/onedarkpro.nvim" },
+  { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+  { src = "https://github.com/nvim-tree/nvim-tree.lua" },
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
+  { src = "https://github.com/nvim-telescope/telescope.nvim" },
+  { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/stevearc/conform.nvim" },
+})
+
+local onedark = require("onedarkpro.helpers")
+local colors = onedark.get_colors("onedark")
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "onedark",
+  callback = function()
+    vim.api.nvim_set_hl(0, "@lsp.type.namespace", { link = "Type" })
+    vim.api.nvim_set_hl(0, "@lsp.type.member", { link = "@function" })
+    vim.api.nvim_set_hl(0, "tsxCloseString", { fg = colors.fg })
+    vim.api.nvim_set_hl(0, "@parameter", { fg = colors.orange })
+    vim.api.nvim_set_hl(0, "typescriptDefaultImportName", { fg = colors.red })
+    vim.api.nvim_set_hl(0, "typescriptIdentifierName", { fg = colors.red })
+    vim.api.nvim_set_hl(0, "typescriptImportBlock", { fg = colors.red })
+    vim.api.nvim_set_hl(0, "typescriptBraces", { fg = colors.fg })
+    vim.api.nvim_set_hl(0, "@function.builtin", { fg = colors.cyan })
+    vim.api.nvim_set_hl(0, "typescriptEndColons", { fg = colors.fg })
+    vim.api.nvim_set_hl(0, "typescriptObjectLabel", { link = "@property" })
+    vim.api.nvim_set_hl(0, "typescriptFuncCallArg", { fg = colors.fg })
+    vim.api.nvim_set_hl(0, "typescriptArrowFunc", { link = "keyword" })
+    vim.api.nvim_set_hl(0, "tsxTagName", { link = "Type" })
+    vim.api.nvim_set_hl(0, "typescriptDecorator", { fg = colors.fg })
+    vim.api.nvim_set_hl(0, "htmlArg", { fg = colors.orange })
+    vim.api.nvim_set_hl(0, "NvimTreeFolderIcon", {})
+    vim.api.nvim_set_hl(0, "NvimTreeRootFolder", { fg = colors.fg })
+    vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderName", { fg = colors.fg })
+    vim.api.nvim_set_hl(0, "NvimTreeFolderName", { fg = colors.fg })
+    vim.api.nvim_set_hl(0, "NvimTreeFolderArrowOpen", { fg = colors.fg })
+    vim.api.nvim_set_hl(0, "NvimTreeFolderArrowClosed", { fg = colors.fg })
+    vim.api.nvim_set_hl(0, "typescriptFuncTypeArrow", { link = "keyword" })
+    vim.api.nvim_set_hl(0, "lessClass", { fg = colors.orange })
+    vim.api.nvim_set_hl(0, "cssPositioningAttr", { fg = colors.fg })
+    vim.api.nvim_set_hl(0, "cssFunctionName", { fg = colors.cyan })
+    vim.api.nvim_set_hl(0, "cssCustomProp", { fg = colors.fg })
+    vim.api.nvim_set_hl(0, "cssUnitDecorators", { fg = colors.red })
+    vim.api.nvim_set_hl(0, "lessFunction", { fg = colors.cyan })
+    vim.api.nvim_set_hl(0, "cssBraces", { fg = colors.orange })
+    vim.api.nvim_set_hl(0, "typescriptCastKeyword", { link = "keyword" })
+    vim.api.nvim_set_hl(0, "typescriptOperator", { link = "keyword" })
+  end,
+})
+
+vim.cmd("colorscheme onedark")
+local statusline = vim.o.statusline
+statusline = statusline:gsub("%%f", "%%F", 1)
+vim.o.statusline = statusline
+
+-- Built-in Treesitter does not auto-attach a highlighter in this config.
+-- Start it on buffer filetype detection so custom captures are visible to :Inspect.
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    pcall(vim.treesitter.start, args.buf)
+  end,
+})
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+require("nvim-tree").setup({
+  renderer = {
+    icons = {
+      show = {
+        git = false, -- 设为 false 即可隐藏 Git 状态图标
+      },
+    },
+  },
+})
+
+-- You dont need to set any of these options. These are the default ones. Only
+-- the loading is important
+require("telescope").setup({
+  extensions = {
+    fzf = {
+      fuzzy = true, -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      -- the default case_mode is "smart_case"
+    },
+  },
+})
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require("telescope").load_extension("fzf")
+local telescope_builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags, { desc = "Telescope help tags" })
+vim.o.autocomplete = true
+vim.o.complete = ".,o,w"
+vim.o.completeopt = "menu,menuone,noselect,popup"
+-- vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, { desc = 'Signature Help' })
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if not client then
+      return
+    end
+    if client:supports_method("textDocument/completion") then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
+})
+
+vim.api.nvim_create_augroup("LspAttachGroup", { clear = true })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = "LspAttachGroup",
+  callback = function(args)
+    local bufnr = args.buf
+    -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, desc = 'Go to Definition' })
+    vim.keymap.set("n", "gd", telescope_builtin.lsp_definitions, { desc = "Go to Definition" })
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "Hover Documentation" })
+    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = bufnr, desc = 'Find References' })
+    vim.keymap.set("n", "gr", telescope_builtin.lsp_references, { desc = "Find References" })
+    -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = bufnr, desc = 'Go to Implementation' })
+    vim.keymap.set("n", "gi", telescope_builtin.lsp_implementations, { desc = "Go to Implementation" })
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename Symbol" })
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code Action" })
+  end,
+})
+
+vim.lsp.enable({ "lua_ls", "vtsls", "basedpyright", "gopls", "clangd", "angularls", "biome" })
+
+require("conform").setup({
+  formatters_by_ft = {
+    go = { "gofumpt", "goimports" },
+    lua = { "stylua" },
+    css = { "biome" },
+    json = { "biome" },
+    jsonc = { "biome" },
+    python = { "isort", "black" },
+    javascript = { "biome" },
+    typescript = { "biome" },
+    javascriptreact = { "biome" },
+    typescriptreact = { "biome" },
+  },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 1000,
+    lsp_format = "fallback",
+  },
+})
